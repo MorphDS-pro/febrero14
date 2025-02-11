495cf994d6ec5235081da0a7f209b802662c4a09
MorphDS-pro <morphds.pro@gmail.com>
2025-02-11 19:07:45 -0300

    Add files via upload

---
+++ loadContent.js
@@ -0,0 +1,25 @@
+export function loadContent(url) {
+  document.getElementById('spinner').style.display = 'flex';
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(data => {
+      console.log('Contenido cargado:', data); 
+      document.getElementById('content').innerHTML = data;
+      document.getElementById('spinner').style.display = 'none'; 
+    })
+    .catch(error => {
+      console.error('Error cargando el contenido:', error);
+      document.getElementById('spinner').style.display = 'none'; 
+      document.getElementById('content').innerHTML = 'Hubo un error al cargar el contenido.'; 
+    });
+}
+
+window.onload = function() {
+  loadContent('portafolio/amor.html');
+};
